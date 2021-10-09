5c939f4ef41cf3dcd4a7d428b7d18167641696c2
idungstanley <sundaystanley56@gmail.com>
2021-10-08 23:31:12 -0700

    my calc

---
+++ object.js
@@ -0,0 +1,19 @@
+// let flavour = prompt("what do you like for easter?", "icecream");
+// let gift = {
+//   snacks: "bread",
+//   drink: "yoghurt",
+//   [flavour]: "vanilla",
+// };
+// console.log(user.snacks);
+// console.log(user.icecream);
+
+// function makeUser(name, age) {
+//   return {
+//     name: name,
+//     age: age,
+//     // ...other properties
+//   };
+// }
+
+// let user = makeUser("John", 30);
+// alert(user.name); // John
